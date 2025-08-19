834f015e1ad1aa6d5782f23cacc8bc3839b5f4a4
rayyan2907 <mrayyan5296@gmail.com>
2025-08-19 14:59:14 +0500

    changes

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import cv2
-from camera import get_frame
-from vision import aruco_utils, pothole_cv, mapping
-from utils import logger, draw
+from camera import capture_image
+from vision import rectification, pothole_cv, mapping
 from control import path_planner
 from mqtt import mqtt_client
 from config import GRID_SIZE, IMAGE_SIZE
 
-log = logger.setup_logger()
+#log = #logger.setup_#logger()
 
 
 def main():
-    mqtt_client.connect()
+    #mqtt_client.connect()
 
     # 1. Capture
-    frame = get_frame()
-    log.info("Frame captured from camera.")
+    frame = capture_image()
+    print("Frame captured from camera.")
 
     # 2. Rectify using ArUco
-    warped = aruco_utils.rectify_frame(frame)
-    log.info("Frame rectified with ArUco.")
+    warped = rectification.rectify_frame(frame)
+    print("Frame rectified with ArUco.")
 
     # 3. Detect pothole
-    pothole_px = pothole_cv.detect_pothole(warped)
+    pothole_px = pothole_cv.detect_potholes(warped)
     if pothole_px is None:
-        log.warning("No pothole detected.")
+        print("No pothole detected.")
         return
-    log.info(f"Pothole detected at pixels {pothole_px}")
+    print(f"Pothole detected at pixels {pothole_px}")
 
     # 4. Convert to grid
     pothole_cell = mapping.pixel_to_grid(pothole_px, IMAGE_SIZE, GRID_SIZE)
-    log.info(f"Pothole mapped to grid {pothole_cell}")
+    print(f"Pothole mapped to grid {pothole_cell}")
 
     # Demo: assume car starts at bottom-center
     start = (GRID_SIZE[1]-1, GRID_SIZE[0]//2)
@@ -38,18 +37,18 @@ def main():
     grid = [[0]*GRID_SIZE[0] for _ in range(GRID_SIZE[1])]
     path = path_planner.plan_path(
         grid, start, (pothole_cell[1], pothole_cell[0]))
-    log.info(f"Path planned with {len(path)} steps.")
+    print(f"Path planned with {len(path)} steps.")
 
     # 6. Draw + show
     overlay = mapping.draw_grid(warped, GRID_SIZE)
-    overlay = pothole_cv.draw_pothole(overlay, pothole_px)
-    overlay = draw.draw_path(overlay, path, GRID_SIZE)
+    overlay = pothole_cv.detect_potholes(overlay, pothole_px)
+    overlay = pothole_cv.draw_potholes(overlay, path, GRID_SIZE)
     cv2.imshow("Result", overlay)
     cv2.waitKey(0)
 
     # 7. Send via MQTT
-    mqtt_client.publish_message({"path": path})
-    log.info("Path sent to car over MQTT.")
+   ## mqtt_client.publish_message({"path": path})
+    #log.info("Path sent to car over MQTT.")
 
 
 if __name__ == "__main__":
